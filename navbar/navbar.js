a2cbb2dfb0d14cd22c9da7f4ec184f6003043ef4
miguelbinue <miguelbprada@gmail.com>
2022-11-25 10:12:28 +0100

    Burger done

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,27 +1,6 @@
-function repliegue() {
-    // if(hide-menu !== null) {
-    //     $("#main-menu-column").addClass("hide-menu");
-    //     $("#main-menu-column").prop("hide-menu", true);
-    // } else {
-    //     $("#main-menu-column").addClass("display-menu");
-    //     $("#main-menu-column").prop("display-menu", true);
-    // }
-
-    let isShown = document.getElementsByClassName('display-menu');
-    let isHidden = document.getElementsByClassName('hide-menu');
-    console.log(isShown);
-    console.log(isHidden);
+function estoyCansao() {
+    document.querySelector("#main-menu-column").classList.toggle("display-menu");
+    console.log("Estoy haciendo cosas");
+    document.querySelector("#navbar").classList.toggle("padding-nav");
     
-    if (isHidden !== null) {
-        console.log("isHidden");
-        $("#main-menu-column").removeClass("hide-menu");
-        $("#main-menu-column").addClass("display-menu");
-        isHidden = null;
-    } else if (isShown !== null) {
-        console.log("isShown");
-        $("#main-menu-column").removeClass("display-menu");
-        $("#main-menu-column").addClass("hide-menu");
-        console.log("he llegado");
-        isShown = null;
-    }
 }
